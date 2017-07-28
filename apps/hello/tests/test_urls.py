e15a6efa3b077f54f2c0223f337cff11013fac15
Artem Khymenko <artem.khymenko@gmail.com>
2017-07-28 15:35:33 +0300

    ticket:1 test_views fail

--- apps/hello/tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.core.urlresolvers import reverse
-
-
-class HelloUrlsTest(TestCase):
-
-    def test_index_url(self):
-        url = reverse('index')
-        self.assertEqual(url, '/')
