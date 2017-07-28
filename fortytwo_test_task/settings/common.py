0bdcce3e7d11023211fe8bf8170427ad5b86e09d
Artem Khymenko <artem.khymenko@gmail.com>
2017-07-28 16:10:31 +0300

    ticket:1 implement view to render index.html(test_views pass)

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -124,6 +124,8 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'templates', 'apps', 'hello')
+
 )
 
 # Turn off south during test
